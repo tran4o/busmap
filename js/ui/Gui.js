dc8ffa75023cfd8cede49d0bc6eb3de5a01477e1
Martin Hristov <tran4o@gmail.com>
2016-12-15 19:34:37 +0200

    ttest

--- js/ui/Gui.js
+++ js/ui/Gui.js
@@ -446,7 +446,7 @@ Class("Gui",
 					if (part.rotation != undefined && part.speed > 0 && part.isWatched && part.avail > 0.5 ) 
 					{
 						// opc,rotation = xr
-						if (typeof window.hackRotation == "undefined") {
+						if (typeof window.hackRotation == "undefined" && true) {
 			        		ctx.rotate(-part.rotation);
 						} else {
 			        		ctx.rotate(window.hackRotation(part.rotation));
