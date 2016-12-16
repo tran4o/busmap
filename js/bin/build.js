07d0a0144b333cc2f93dd8e08f861b80a8ace697
VisionR <visionr@bot.bot>
2016-12-16 08:35:34 +0200

    daasda

--- js/bin/build.js
+++ js/bin/build.js
@@ -4,7 +4,9 @@ var log=require("../misc/log");
 var fs=require("fs");
 var inst = require("../conf/installation.js");
 var mkdirp = require('mkdirp');
+
 mkdirp.sync(inst.installDir+"/tmp");
+
 var browserify = require('browserify');
 var watchify = require('watchify');
 
@@ -15,9 +17,12 @@ var b1 = browserify({
   packageCache: {},
   plugin: [watchify]
 });
+
 b1.plugin('minifyify', {output: inst.installDir+"/tmp/t1.js.dbg",map: inst.installDir+"/tmp/t1.js.map"});
 b1.on('update', bundle1);
+
 bundle1();
+
 var b2 = browserify({
 	debug: true,
 	entries: [inst.installDir+"/js/ui/UI.js"],
@@ -25,22 +30,26 @@ var b2 = browserify({
 	packageCache: {},
 	plugin: [watchify]
 });
+
 b2.on('update', bundle2);
 b2.plugin('minifyify', {output: inst.installDir+"/tmp/t2.js.dbg",map: inst.installDir+"/tmp/t2.js.map"});
+
 bundle2();
+
 function bundle1() {
 	try {
 		console.log("Bundle "+inst.installDir+"/js/api/busmap.js"+" to "+inst.installDir+"/tmp/t1.js");
 		b1.bundle().pipe(fs.createWriteStream(inst.installDir+"/tmp/t1.js"));
 	} catch (e) {
-		log.error("OPS!");
+		log.error("OPS:" + e);
 	}
 }
+
 function bundle2() {
 	try {
 		console.log("Bundle "+inst.installDir+"/js/ui/UI.js"+" to "+inst.installDir+"/tmp/t2.js");
 		b2.bundle().pipe(fs.createWriteStream(inst.installDir+"/tmp/t2.js"));
 	} catch (e) {
-		log.error("OPS!");
+		log.error("OPS: " + e);
 	}
 }
