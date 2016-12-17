c75354bc5e92e045813f5b40db63a87b02f60e26
Martin Hristov <tran4o@gmail.com>
2016-12-17 11:52:47 +0200

    kor

--- js/ui/Styles.js
+++ js/ui/Styles.js
@@ -291,7 +291,7 @@ var STYLES=
 				image : new ol.style.Icon({
 					src : "/img/"+icn.image,
 					scale : (icn.scale || 1),
-					opacity : 0.34
+					opacity : 0.27
 				}),
 				geometry: new ol.geom.Point(p)
 			 }));
